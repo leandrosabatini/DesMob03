0b5c2a12496e643c3ad93937e5f68522b0e8f646
Leandro Sabatini <contato@leandrosabatini.com.br>
2020-03-26 22:16:49 -0300

    Realizando alterações solicitadas como exercício da aula 04

--- App.js
+++ App.js
@@ -1,100 +1,56 @@
-import { FlatList, StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+
+import ContatoItem from './components/ContatoItem';
+import ContatoInput from './components/ContatoInput';
 
 const styles = StyleSheet.create({
     telaPrincipalView: {
         padding: 50
-    },
-    contatoView: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: "center"
-    },
-    nomeInputText: {
-        width: '80%',
-        borderBottomColor: 'black',
-        borderBottomWidth: 1,
-        padding: 2,
-        marginBottom: 20
-    },
-    telefoneInputText: {
-        width: '80%',
-        borderBottomColor: 'black',
-        borderBottomWidth: 1,
-        padding: 2
-    },
-    itemNaLista: {
-        padding: 12,
-        backgroundColor: '#CCC',
-        borderColor: '#000',
-        borderWidth: 1,
-        marginBottom: 8,
-        borderRadius: 8
     }
 });
 
 export default function App() {
-    const [nome, setNome] = useState('');
-    const [celular, setCelular] = useState('');
     const [contatos, setContatos] = useState('');
-    const [contadorcontatos, setContadorcontatos] = useState(10);
+    const [contadorContatos, setContadorcontatos] = useState(10);
 
-    const capturarNome = (nome) => {
-        setNome(nome)
+    const removerContato = (keyToRemove) => {
+        setContatos (contatos => {
+            return contatos.filter((contato) => {
+                return contato.key !== keyToRemove
+            })
+        });
     };
 
-    const capturarCelular = (celular) => {
-        setCelular(celular)
-    };
-
-    const adicionarContato = () => {
+    const adicionarContato = (nome, celular) => {
         setContatos((contatos) => {
-            setContadorcontatos(contadorcontatos + 2);
+            setContadorcontatos(contadorContatos + 2);
             return [
                 ...contatos,
                 {
-                    key: contadorcontatos.toString(),
+                    key: contadorContatos.toString(),
                     nome: nome,
                     celular: celular
                 }
             ];
         } );
-
-        setNome('');
-        setCelular('');
     }
 
     return (
         <View style={styles.telaPrincipalView}>
-            <View style={styles.contatoView}>
-                <TextInput
-                    placeholder="Nome"
-                    style={styles.nomeInputText}
-                    onChangeText={capturarNome}
-                    value={nome}
-                />
-            </View>
-            <View style={styles.contatoView}>
-                <TextInput
-                    placeholder="Telefone"
-                    style={styles.telefoneInputText}
-                    onChangeText={capturarCelular}
-                    value={celular}
-                />
-                <Button
-                    title="+"
-                    onPress={adicionarContato}
-                />
+            <View>
+                <ContatoInput onAdicionarContato={adicionarContato} />
             </View>
             <FlatList
                 data={contatos}
                 renderItem={
-                    lembrete => (
-                        <View style={styles.itemNaLista}>
-                            <Text>#{lembrete.item.key}</Text>
-                            <Text>Nome: {lembrete.item.nome}</Text>
-                            <Text>Celular: {lembrete.item.celular}</Text>
-                        </View>
+                    contato => (
+                        <ContatoItem
+                            chave={contato.item.key}
+                            nome={contato.item.nome}
+                            celular={contato.item.celular}
+                            onDelete={removerContato}
+                        />
                     )
                 }
             />
